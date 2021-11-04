045c2802310eea69e6ffd86f18dd6de77304b203
Bryan Chasko <reddythafox@hotmail.com>
2021-11-04 00:11:24 -0600

    organizing
    organizing into OOP.

--- Main.cpp
+++ Main.cpp
@@ -14,21 +14,6 @@ windows, contextsand surfaces, receiving inputand events.
 */
 #include<GLFW/glfw3.h>
 
-//Victor Gordan fragment shader code for constructing
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\n\0";
-
 // main function initializing our program
 int main()
 {
